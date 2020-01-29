a9588446b29591508dba8bf2a33651406ac197a9
Kuni <kuni.ujiie@grapecity.com>
2020-01-29 15:26:22 +0900

    プロジェクト ファイルを追加します。

---
+++ DDExportFormData/Program.cs
@@ -0,0 +1,28 @@
+﻿using GrapeCity.Documents.Pdf;
+using System;
+using System.IO;
+
+namespace DDExportFormData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("DioDocs for PDF. PDF Form Data Export!");
+
+            var doc = new GcPdfDocument();
+
+            // PDFを読み込み
+            doc.Load(new FileStream("grapecity_order_embed.pdf", FileMode.Open, FileAccess.Read));
+
+            // FDFファイルとしてデータを出力
+            doc.ExportFormDataToFDF("FormData_FDF.fdf");
+
+            // XFDFファイルとしてデータを出力
+            doc.ExportFormDataToXFDF("FormData_XFDF.xfdf");
+
+            // XMLファイルとしてデータを出力
+            doc.ExportFormDataToXML("FormData_XML.xml"); 
+        }
+    }
+}
